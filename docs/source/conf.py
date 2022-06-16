5ed75a6f1391bb906405e9925018966be0b66213
Andreas Timoudas <andreas.timoudas@STO-FVFGMAW6Q05D.local>
2022-06-16 11:20:48 +0200

    added project/authot

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'KintoFortnox Documentation'
+copyright = '2022, Axenon'
+author = 'Andreas Timoudas'
 
 release = '0.1'
 version = '0.1.0'
